1e137e34d975a89523d1b68ff769c1ae21c4eff7
hezongjiang <422736262@qq.com>
2022-05-13 21:35:54 +0800

    feat: 项目初始化

---
+++ src/main/java/com/tencent/food/recommend/FoodRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.tencent.food.recommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodRecommendApplication.class, args);
+    }
+
+}
