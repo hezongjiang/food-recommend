eeee77ac11baa270c827ae4cfc1c4ba5ab2b0785
hezongjiang <422736262@qq.com>
2022-05-18 21:56:38 +0800

    Update WXConstant.java

--- src/main/java/com/tencent/food/recommend/common/consts/WXConstant.java
+++ src/main/java/com/tencent/food/recommend/common/consts/WXConstant.java
@@ -2,7 +2,7 @@ package com.tencent.food.recommend.common.consts;
 
 public class WXConstant {
 
-    public static final String APPID = "";
+    public static final String APPID = "wx336e75821280cba5";
 
-    public static final String SECRET = "";
+    public static final String SECRET = "b34d9fd09cfc04fe9b4643e9c3645461";
 }
