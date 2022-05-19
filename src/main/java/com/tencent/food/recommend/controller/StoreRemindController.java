6914e400b7da54c20c4e9725d2036323781863b3
nathan <2551722670@qq.com>
2022-05-19 17:32:33 +0800

    添加囤货提醒

--- src/main/java/com/tencent/food/recommend/controller/StoreRemindController.java
+++ src/main/java/com/tencent/food/recommend/controller/StoreRemindController.java
@@ -2,15 +2,18 @@ package com.tencent.food.recommend.controller;
 
 import com.tencent.food.recommend.common.ResultData;
 import com.tencent.food.recommend.common.consts.WXConstant;
+import com.tencent.food.recommend.common.enums.ReturnCode;
 import com.tencent.food.recommend.persist.model.StoreRemind;
 import com.tencent.food.recommend.response.StoreRemindResponse;
 import com.tencent.food.recommend.service.StoreRemindService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/storeRemind")
+@Slf4j
 public class StoreRemindController {
 
     @Autowired
@@ -19,18 +22,26 @@ public class StoreRemindController {
     StoreRemindResponse storeRemindResponse;
 
     @PostMapping("/add")
-    public ResultData addRemind (//@CookieValue(name = WXConstant.OPEN_ID, required = true) String openId,
+    public ResultData addRemind (@RequestHeader(name = "openid") String openId,
                                  @RequestParam(value = "remindDate") Long remindDate,
                                  @RequestParam(value = "remarks") String remarks) {
         //先代替openid
-        String openId = "1";
+        //String openId = "1";
 
         storeRemind = new StoreRemind();
         storeRemind.setCreatedDate(System.currentTimeMillis());
         storeRemind.setRemindDate(remindDate);
         storeRemind.setRemarks(remarks);
+        log.info("此时的openid为：" + openId);
         int result = storeRemindService.add(openId,storeRemind);
-        return ResultData.success(storeRemind);
+        if (result == 1) {
+            log.info("添加囤货提醒成功");
+            return ResultData.success(storeRemind);
+        } else {
+            log.info("添加囤货提醒失败");
+            return ResultData.fail(ReturnCode.RC999.getCode(), "插入失败");
+        }
+
 
     }
     @GetMapping("/find")
