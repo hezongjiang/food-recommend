ce6439aeecad19d06631dc59a75bf4dbb54739e0
hezongjiang <422736262@qq.com>
2022-05-23 20:21:12 +0800

    feat：提取文件路径

--- src/main/java/com/tencent/food/recommend/controller/FileController.java
+++ src/main/java/com/tencent/food/recommend/controller/FileController.java
@@ -1,6 +1,7 @@
 package com.tencent.food.recommend.controller;
 
 import com.tencent.food.recommend.common.ResultData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +17,20 @@ import java.io.IOException;
 @RequestMapping("upload")
 public class FileController {
 
+    @Value("${image.path}")
+    private String imagePath;
+
     @PostMapping(value = "image")
     public ResultData<String> fileUpload(@RequestParam(value = "file") MultipartFile file, Model model, HttpServletRequest request) {
         if (file.isEmpty()) {
-            System.out.println("文件为空空");
             return ResultData.success("文件为空空");
         }
         String fileName = file.getOriginalFilename();  // 文件名
+        if (fileName == null || fileName.length() == 0) {
+            return ResultData.success("文件名为空");
+        }
         String suffixName = fileName.substring(fileName.lastIndexOf("."));  // 后缀名
-        String filePath = "/Users/hezongjiang/Desktop/temp/"; // 上传后的路径
+        String filePath = imagePath; // 上传后的路径
         fileName = System.currentTimeMillis() + suffixName; // 新文件名
         File dest = new File(filePath + fileName);
         if (!dest.getParentFile().exists()) {
