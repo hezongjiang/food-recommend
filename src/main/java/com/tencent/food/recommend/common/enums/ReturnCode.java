5da123fa6bf3b503e1354366ef80b5df6ca1bc23
nathan <2551722670@qq.com>
2022-05-24 20:45:48 +0800

    修改返回值枚举中状态码为Integer类型

--- src/main/java/com/tencent/food/recommend/common/enums/ReturnCode.java
+++ src/main/java/com/tencent/food/recommend/common/enums/ReturnCode.java
@@ -22,10 +22,13 @@ public enum ReturnCode {
     ACCESS_DENIED(2003,"没有权限访问该资源"),
     CLIENT_AUTHENTICATION_FAILED(1001,"客户端认证失败"),
     USERNAME_OR_PASSWORD_ERROR(1002,"用户名或密码错误"),
-    UNSUPPORTED_GRANT_TYPE(1003, "不支持的认证模式");
+    UNSUPPORTED_GRANT_TYPE(1003, "不支持的认证模式"),
+
+
+    USER_NOT_EXISTS(3001, "用户不存在");
 
     /**自定义状态码**/
-    private final int code;
+    private final Integer code;
     /**自定义描述**/
     private final String message;
 
